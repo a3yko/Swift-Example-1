8370561da0ac272402657ce64509e62735760a61
Adrian Atanasov <adriansoccar@gmail.com>
2019-09-02 16:14:23 -0500

    New program

---
+++ Swift Example 1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Example 1
+//
+//  Created by Adrian Atanasov on 9/2/19.
+//  Copyright © 2019 Adrian Atanasov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
