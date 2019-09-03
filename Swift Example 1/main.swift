87c7d8e3074473f8d428c527291f9aedbca49e64
Adrian Atanasov <adriansoccar@gmail.com>
2019-09-02 20:51:15 -0500

    Added types

--- Swift Example 1/main.swift
+++ Swift Example 1/main.swift
@@ -8,17 +8,17 @@
 
 import Foundation
 
-let sample1 = 0x3A
-let sample2 = 58
+let sample1:UInt8 = 0x3A
+let sample2:UInt8 = 58
 var heartRate = 85
-var deposits = 135002796
-let acceleration = 9.800
-var mass = 14.6
-var distance = 129.763001
+var deposits:Double = 135002796
+let acceleration:Float = 9.800
+var mass:Float = 14.6
+var distance:Double = 129.763001
 var lost = true
 var expensive = true
 var choice = 2
-let integral = "\u{222B}"
+let integral:Character = "\u{222B}"
 let greeting = "Hello"
 var name = "Karen"
 var force = mass*acceleration
